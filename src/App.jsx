a1c854dc9712ad7f104b1bc5e228295fb2dc2280
ljadow <lindsay@jadow.com>
2023-12-08 16:15:42 -0500

    finished the workshop

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,29 @@ import { puppyList } from './data'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  
+  console.log(featuredPup)
+  // console.log(puppies)
 
   return (
     <div className="App">
       {
-        puppies.map((puppy)=>{
-          return <p key={puppy.id}>{puppy.name}</p>
+        puppies.map((puppy) => {
+          return <p className = "pup" onClick={() => { setFeatPupId(puppy.id) }}key={puppy.id}>{puppy.name}</p>
         })
       }
+      
+      {featPupId && (
+          <div className="featpup">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+      )}
     </div>
   )
 }
